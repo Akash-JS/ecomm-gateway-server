9dd03419060e91da729c2b331caeb6c5dd58aa65
Akash Patel <aksp1790@gmail.com>
2024-05-06 11:58:08 +0530

    initial commit

---
+++ src/test/java/org/example/ecommgatewayserver/EcommGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecommgatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
